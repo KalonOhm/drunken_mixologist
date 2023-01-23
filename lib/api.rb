be971193b712ebe394a72db64dc5e4991ccaaac5
Kalon Ohmstede <106362195+KalonOhm@users.noreply.github.com>
2023-01-23 13:30:51 -0600

    Create api.rb

---
+++ lib/api.rb
@@ -0,0 +1,7 @@
+
+# this is the API page I found
+# https://www.thecocktaildb.com/api.php
+
+API_KEY = "1"
+# default API key for development and educational use 
+# patreon support ($2) unlocks additional features and private API_KEY
